c2189e4ca28f44d3f7f441f44f2fd6dc58a9a49d
Skradski <tkskradski@gmail.com>
2018-10-16 21:42:05 -0400

    Initial Commit - Liquibase configured

---
+++ src/main/java/com/radar/achilles/AchillesApplication.java
@@ -0,0 +1,15 @@
+package com.radar.achilles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Skradski on 10/16/2018.
+ */
+
+@SpringBootApplication
+public class AchillesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AchillesApplication.class, args);
+    }
+}
