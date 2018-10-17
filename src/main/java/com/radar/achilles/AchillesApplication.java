b9b087157868b2b8857d433df9cde9e059fe826f
Skradski <tkskradski@gmail.com>
2018-10-16 22:43:24 -0400

    Integrating MVC.  Examples added.

--- src/main/java/com/radar/achilles/AchillesApplication.java
+++ src/main/java/com/radar/achilles/AchillesApplication.java
@@ -9,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AchillesApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(AchillesApplication.class, args);
     }
+
 }
